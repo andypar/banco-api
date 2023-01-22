1eb168fa49644a2a06f431480e5a3e6f0e5e9b46
andypar <andypar@users.noreply.github.com>
2023-01-22 13:58:12 -0300

    migracion usuarios y productos prueba

--- migrations/1673921136432-initial_migration.js
+++ migrations/1673921136432-initial_migration.js
@@ -1,53 +1,69 @@
 const { models } = require("../db");
+const mongoose = require("mongoose");
+//const Schema = mongoose.Schema;
+//const { ObjectId } = mongoose.Schema.Types.ObjectId;
+
 
 async function up() {
   await models.GenderType.create([
     {
+      _id: new mongoose.Types.ObjectId('000000000000000000000000'),
       description: "f",
     },
     {
+      _id: new mongoose.Types.ObjectId('000000000000000000000001'),
       description: "m",
     },
     {
+      _id: new mongoose.Types.ObjectId('000000000000000000000002'),
       description: "x",
     },
   ])
 
   await models.CurrencyType.create([
     {
+      _id: new mongoose.Types.ObjectId('000000000000000000000000'),
       description: "ars",
     },
     {
+      _id: new mongoose.Types.ObjectId('000000000000000000000001'),
       description: "usd",
     },
   ])
 
   await models.MovementType.create([
     {
+      _id: new mongoose.Types.ObjectId('000000000000000000000000'),
       description: "transferencia",
     },
     {
+      _id: new mongoose.Types.ObjectId('000000000000000000000001'),
       description: "deposito",
     },
     {
+      _id: new mongoose.Types.ObjectId('000000000000000000000002'),
       description: "extraccion",
     },
   ])
 
   await models.ProductType.create([
     {
+      _id: new mongoose.Types.ObjectId('000000000000000000000000'),
       description: "ca",
     },
     {
+      _id: new mongoose.Types.ObjectId('000000000000000000000001'),
       description: "cc",
     },
   ])
 
   await models.PersonType.create([
     {
+      _id: new mongoose.Types.ObjectId('000000000000000000000000'),
       description: "f",
     },
     {
+      _id: new mongoose.Types.ObjectId('000000000000000000000001'),
       description: "j",
     },
   ])
@@ -55,23 +71,23 @@ async function up() {
 
 async function down() {
   await models.GenderType.deleteMany({
-    description: ["f", "m", "x"],
+    _id: ['000000000000000000000000','000000000000000000000001', '000000000000000000000002'],
   });
 
   await models.CurrencyType.deleteMany({
-    description: ["ars", "usd"],
+    _id: ['000000000000000000000000','000000000000000000000001'],
   });
 
   await models.MovementType.deleteMany({
-    description: ["transferencia", "deposito", "extraccion"],
+    _id: ['000000000000000000000000','000000000000000000000001', '000000000000000000000002'],
   });
 
   await models.ProductType.deleteMany({
-    description: ["ca", "cc"],
+    _id: ['000000000000000000000000','000000000000000000000001'],
   });
 
   await models.PersonType.deleteMany({
-    description: ["f", "j"],
+    _id: ['000000000000000000000000','000000000000000000000001'],
   });
 }
 
---
+++ migrations/1674355243674-user_product.js
@@ -0,0 +1,132 @@
+const { models } = require("../db");
+const mongoose = require("mongoose");
+
+async function up() {
+  await models.User.create([
+    {
+      _id: new mongoose.Types.ObjectId('000000000000000000000000'),
+      name: {
+        firstName: "Andrea Paula",
+        lastName: "Rodriguez",
+      },
+      gender: "000000000000000000000000", //f
+      dni: "34401327",
+      dateBirth: "1988-12-29",
+      email: "andypar29@hotmail.com",
+      password: "Perritos123!!",
+      username: "andypar29",
+      telephone: "1564078964",
+      personType: "000000000000000000000000", //f
+      cuilCuit: "27-34401327-1",
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      _id: new mongoose.Types.ObjectId('000000000000000000000001'),
+      name: {
+        firstName: "Andrea Paula",
+        lastName: "Rodriguez",
+      },
+      gender: "000000000000000000000000", //f
+      dni: "34401328",
+      dateBirth: "1988-12-28",
+      email: "andypar28@hotmail.com",
+      password: "Perritos123!!",
+      username: "andypar28",
+      telephone: "1564078964",
+      personType: "000000000000000000000000", //f
+      cuilCuit: "27-34401328-1",
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      _id: new mongoose.Types.ObjectId('000000000000000000000002'),
+      name: {
+        firstName: "Andrea Paula",
+        lastName: "Rodriguez",
+      },
+      gender: "000000000000000000000000", //f
+      dni: "34401330",
+      dateBirth: "1988-12-30",
+      email: "andypar30@hotmail.com",
+      password: "Perritos123!!",
+      username: "andypar30",
+      telephone: "1564078964",
+      personType: "000000000000000000000000", //f
+      cuilCuit: "27-34401330-1",
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+  ]);
+
+  await models.Product.create([
+    {
+      _id: new mongoose.Types.ObjectId('000000000000000000000000'),
+      type: '000000000000000000000000', //ca
+      currency: '000000000000000000000000', //ars
+      accountNumber: '0929/01110051/25',
+      cbu: '0150929201000110051256',
+      alias: 'andypar29',
+      balanceAmount: '100000',
+      overdraftAmount: '10000',
+      extractionLimit: '10000',
+      movements: [],
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      _id: new mongoose.Types.ObjectId('000000000000000000000001'),
+      type: '000000000000000000000000', //ca
+      currency: '000000000000000000000001', //usd
+      accountNumber: '0929/01110051/26',
+      cbu: '0150929201000110051266',
+      alias: 'andypar28',
+      balanceAmount: '10000',
+      overdraftAmount: '1000',
+      extractionLimit: '1000',
+      movements: [],
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      _id: new mongoose.Types.ObjectId('000000000000000000000002'),
+      type: '000000000000000000000001', //cc
+      currency: '000000000000000000000000', //ars
+      accountNumber: '0929/01110051/27',
+      cbu: '0150929201000110051276',
+      alias: 'andypar30',
+      balanceAmount: '0',
+      overdraftAmount: '10000',
+      extractionLimit: '10000',
+      movements: [],
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      _id: new mongoose.Types.ObjectId('000000000000000000000003'),
+      type: '000000000000000000000000', //ca
+      currency: '000000000000000000000000', //ars
+      accountNumber: '0929/01110051/28',
+      cbu: '0150929201000110051286',
+      alias: 'andypar31',
+      balanceAmount: '150000',
+      overdraftAmount: '10000',
+      extractionLimit: '10000',
+      movements: [],
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+  ]);
+}
+
+async function down() {
+  await models.User.deleteMany({
+    _id: ["000000000000000000000000", "000000000000000000000001", "000000000000000000000002"],
+  });
+
+  await models.Product.deleteMany({
+    _id: ["000000000000000000000000", "000000000000000000000001", "000000000000000000000002", , "000000000000000000000003"],
+  });
+}
+
+module.exports = { up, down };
